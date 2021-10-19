3606b4f33e0ebc8e3c83bea938959adf8b74ac19
Evgeniia Mestnikova <zhenyamestnik@gmail.com>
2021-10-19 21:03:13 +1000

    finished 3N.py

--- 3N.py
+++ 3N.py
@@ -1,11 +1,22 @@
-'''with open('input.txt', 'r') as fin:
-    N = fin.readline()
-    a = list(map(int, fin.read().split()))'''
+with open('input.txt', 'r') as fin:
+    N = int(fin.readline())
+    a = list(map(str, fin.read().split()))
 
-N = int(input())
-a = list(map(int, input().split()))
-b = [].append(a[0])
+b = []
+d = {}
 
-for i in range(1, len(a)):
-    if str(a[i]).lower() in :
+for i in range(len(a)):
+    if a[i].lower() not in d:
+        b.append(a[i])
+        d[a[i].lower()] = 1
+    else:
+        last_digit = d[a[i].lower()]
+        d[a[i].lower()] += 1
+        while a[i].lower() + str(last_digit) in d:
+            last_digit += 1
+        a[i] = a[i] + str(last_digit)
+        d[a[i].lower()] = 1
+        b.append(a[i])
 
+with open('output.txt', 'w') as fout:
+    fout.write('\n'.join(b))
