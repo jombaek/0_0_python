be293dc103f556421d0920c14cceec2591bb65a1
Evgeniia Mestnikova <zhenyamestnik@gmail.com>
2021-10-22 17:46:04 +1000

    modified 4L.py and 4M.py

--- 4L.py
+++ 4L.py
@@ -1,4 +1,1 @@
-#a = list(map(int, input().split())); print(' '.join(map(str, [a[i] ** 2 for i in range(len(a) - 1, -1, -1) if a[i] > 0])))
-
-
 print(' '.join(map(str, list(reversed([(int(x) ** 2) for x in input().split() if int(x) > 0])))))
--- 4M.py
+++ 4M.py
@@ -1,3 +1,2 @@
 a = list(map(int, input().split()))
 print(' '.join(map(str, a[:2] + list(reversed(a[2:-2])) + a[-2:])))
-
